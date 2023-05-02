242b9aa848b25d2d1557010b8e17846518cbde29
Andrieli <andrieliluci@gmail.com>
2023-05-02 08:15:55 -0300

    chore: atualizar timezone

--- literabito/setup/settings.py
+++ literabito/setup/settings.py
@@ -91,15 +91,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+# definição de timezone (para o Brasil)
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
-
 USE_TZ = True
 
 
