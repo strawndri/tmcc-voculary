acd8a75725295fa98727bb9163bf60e19bcb8768
Andrieli <andrieliluci@gmail.com>
2023-05-02 21:59:19 -0300

    fix: arquivos estáticos não reconhecidos

--- literabito/setup/settings.py
+++ literabito/setup/settings.py
@@ -98,12 +98,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# referenciando a pasta de arquivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
