e719b4ea06447f9a9903060e994527dce7640a54
Andrieli <andrieliluci@gmail.com>
2023-08-05 21:48:48 -0300

    feat: adicionar paginação

--- voculary/meus_arquivos/views.py
+++ voculary/meus_arquivos/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from django.core.paginator import Paginator
+
+from home.models import Imagem, ArquivoDigitalizado
+
 @login_required(login_url='/login')
 def meus_arquivos(request):
-    return render(request, 'meus_arquivos/index.html')
+
+    imagens = Imagem.objects.all()
+    textos = ArquivoDigitalizado.objects.all()
+
+    # paginação
+    paginator = Paginator(imagens, 5)
+    page = request.GET.get('page')
+    imagens = paginator.get_page(page)
+
+    return render(request, 'meus_arquivos/index.html', {'imagens': imagens})
