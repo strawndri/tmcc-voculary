06893874597d6161ff42773c1258c46c991116c2
Andrieli <andrieliluci@gmail.com>
2023-10-14 15:16:44 -0300

    feat: configurar rotas da app usuario

--- voculary/usuario/urls.py
+++ voculary/usuario/urls.py
@@ -1,18 +1,18 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.CadastroView, name='cadastro'),
-    path('login/', views.LoginView, name='login'),
-    path('perfil/', views.PerfilView, name='perfil'),
-    path('logout/', views.LogoutView, name='logout'),
+    re_path(r'^cadastro/?$', views.CadastroView, name='cadastro'),
+    re_path(r'^login/?$', views.LoginView, name='login'),
+    re_path(r'^perfil/?$', views.PerfilView, name='perfil'),
+    re_path(r'^logout/?$', views.LogoutView, name='logout'),
     
     # Grupo de redefinição de senha
-    path('redefinir-senha/', auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path('redefinir-senha/concluido/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('redefinir-senha/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('redefinir-senha/concluido/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    re_path(r'^redefinir-senha/?$', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    re_path(r'^redefinir-senha/concluido/?$', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    re_path(r'^redefinir-senha/<uidb64>/<token>/?$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    re_path(r'^redefinir-senha/concluido/?$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
-    path('reativar/<int:user_id>/<str:token>/', views.ReativarContaView, name='reactivate_account'),
+    re_path(r'^reativar/<int:user_id>/<str:token>/?$', views.ReativarContaView, name='reactivate_account'),
 ]
