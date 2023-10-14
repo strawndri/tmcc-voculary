66da03c10c3ebce5c8b9995c539c0e133a5a802b
Andrieli <andrieliluci@gmail.com>
2023-10-14 16:12:21 -0300

    feat: adicionar responsividade na app gerenciamento_texto

--- voculary/setup/static/js/abrirMenu.js
+++ voculary/setup/static/js/abrirMenu.js
@@ -7,25 +7,25 @@ document.addEventListener("DOMContentLoaded", function() {
     var isOpen = localStorage.getItem('menuOpen');
     if (isOpen === 'true') {
         menu.classList.add('open');
-        nav.style.alignItems = 'flex-start';
-        padding.style.paddingLeft = '20rem';
+        if (window.innerWidth > 768) {
+            padding.style.paddingLeft = '20rem';
+        }
     } else {
         menu.classList.remove('open');
         padding.style.paddingLeft = '8rem';
-        nav.style.alignItems = 'center';
     }
 
     button.addEventListener('click', function() {
         menu.classList.toggle('open');
         
-        if (menu.classList.contains('open')) {
-            padding.style.paddingLeft = '20rem';
-            nav.style.alignItems = 'flex-start';
-            localStorage.setItem('menuOpen', 'true');
-        } else {
-            padding.style.paddingLeft = '8rem';
-            nav.style.alignItems = 'center';
-            localStorage.setItem('menuOpen', 'false');
+        if (window.innerWidth > 768) {
+            if (menu.classList.contains('open')) {
+                padding.style.paddingLeft = '20rem';
+                localStorage.setItem('menuOpen', 'true');
+            } else {
+                padding.style.paddingLeft = '8rem';
+                localStorage.setItem('menuOpen', 'false');
+            }
         }
     });
 });
