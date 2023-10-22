7bd2834285b419a28746145d7d57d6b334bb14eb
Andrieli <andrieliluci@gmail.com>
2023-10-22 08:38:23 -0300

    feat: modificar e-mail de 'esqueci minha senha'

--- voculary/usuario/urls.py
+++ voculary/usuario/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     re_path(r'^logout/?$', views.LogoutView, name='logout'),
     
     # Grupo de redefinição de senha
-    re_path(r'^redefinir-senha/?$', auth_views.PasswordResetView.as_view(), name="password_reset"),
-    re_path(r'^redefinir-senha/concluido/?$', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    re_path(r'^redefinir-senha/<uidb64>/<token>/?$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    re_path(r'^redefinir-senha/concluido/?$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('redefinir-senha/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('redefinir-senha/concluido/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('redefinir-senha/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('redefinir-senha/concluido/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
-    re_path(r'^reativar/<int:user_id>/<str:token>/?$', views.ReativarContaView, name='reactivate_account'),
+    path('reativar/<int:user_id>/<str:token>/', views.ReativarContaView, name='reactivate_account'),
 ]
