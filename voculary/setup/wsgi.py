56d578789a028003fa09a9aa0e653fadf91d51da
Andrieli <andrieliluci@gmail.com>
2023-10-27 20:48:18 -0300

    refactor: adicionar comentários ao arquivo 'wsgi.py' da app 'setup'

--- voculary/setup/wsgi.py
+++ voculary/setup/wsgi.py
@@ -1,16 +1,20 @@
 """
-WSGI config for setup project.
+Configuração WSGI para o projeto "setup".
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+Este arquivo expõe a chamada WSGI como uma variável de nível de módulo chamada "application".
 
-For more information on this file, see
+Para obter mais informações sobre este arquivo, consulte:
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+# Importa os módulos necessários
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Define o módulo de configurações padrão para o Django.
+# Isso permite ao Django saber onde procurar as configurações quando o projeto for iniciado.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
+# Cria a aplicação WSGI usando as configurações padrão do Django.
+# Isso permite ao servidor WSGI servir o projeto Django.
 application = get_wsgi_application()
